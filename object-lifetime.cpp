94de91cd85b029f33316fa190f2ab8ee6966d2ac
neingeist <neingeist@bl0rg.net>
2014-04-13 10:12:03 +0200

    add object lifetime example

---
+++ object-lifetime.cpp
@@ -0,0 +1,25 @@
+#include <cstdio>
+
+struct A {
+   A() { puts("A()"); }
+  ~A() { puts("~A()"); }
+};
+
+struct B {
+   B() { puts("B()"); }
+  ~B() { puts("~B()"); }
+};
+
+struct C {
+  A a;
+  B b;
+
+   C() { puts("C()"); }
+  ~C() { puts("~C()"); }
+};
+
+int main() {
+  C c;
+  // Destructors should be called in exactly the opposite order of the
+  // constructor calls.
+}
