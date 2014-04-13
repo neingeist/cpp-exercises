04851b7e5f85ea3705345aa6fe9a6436df902100
neingeist <neingeist@bl0rg.net>
2014-04-13 08:38:06 +0200

    add auto_ptr.cpp

---
+++ auto_ptr.cpp
@@ -0,0 +1,19 @@
+#include <cassert>
+#include <iostream>
+#include <memory>
+
+int main() {
+    int *i = new int;
+    std::auto_ptr<int> x(i);
+    std::auto_ptr<int> y;
+    /* Note: auto_ptr is deprecated. */
+
+    y = x;
+
+    std::cout << x.get() << std::endl; // Print NULL
+    assert(x.get() == NULL);
+    std::cout << y.get() << std::endl; // Print non-NULL address i
+    assert(y.get() != NULL);
+
+    return 0;
+}
