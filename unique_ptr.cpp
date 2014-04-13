6463aab35ccc138cd8abc26f336eec1d254122bd
neingeist <neingeist@bl0rg.net>
2014-04-13 10:51:19 +0200

    add a simple example to unique_ptr.cpp

--- unique_ptr.cpp
+++ unique_ptr.cpp
@@ -3,16 +3,35 @@
 #include <memory>
 #include <vector>
 
-int main(void) {
+void simple() {
+  // From Wikipedia.
+
+  std::unique_ptr<int> p1(new int(5));
+
+  // Compile error - can't copy p1!
+  // std::unique_ptr<int> p2 = p1;
+
+  assert(p1 != NULL);
+  // Transfers ownership. p3 now owns the memory and p1 is rendered invalid:
+  std::unique_ptr<int> p3 = std::move(p1);
+  assert(p1 == NULL && p3 != NULL);
+}
+
+void container() {
   std::vector<std::unique_ptr<int>> v;
   std::unique_ptr<int> q(new int(42));
 
   assert(q != NULL);
-  // v.push_back(q);
+  // v.push_back(q); <- Compile error
   v.push_back(std::move(q));
   assert(q == NULL);
 
   for(auto &e: v) {
     std::cout << *e << std::endl;
   }
+}
+
+int main(void) {
+  simple();
+  container();
 }
