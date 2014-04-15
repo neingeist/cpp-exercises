9b2aef1701b18f883a4f3534c33d82a036f727c8
neingeist <neingeist@bl0rg.net>
2014-04-15 20:35:17 +0200

    shared_ptr vs containers

--- shared_ptr.cpp
+++ shared_ptr.cpp
@@ -1,5 +1,6 @@
 #include <cassert>
 #include <memory>
+#include <vector>
 
 // From Wikipedia.
 
@@ -19,6 +20,24 @@ void shared_ptr() {
   assert(p1.get() == NULL && p2.get() == NULL);
   assert(p2.use_count() == 0);
   assert(p1.use_count() == 0);
+}
+
+void shared_ptr_container() {
+  std::shared_ptr<int> p1(new int(5));
+  std::shared_ptr<int> p2 = p1; // Both now own the memory.
+  assert(p1.use_count() == 2);
+
+  std::vector<std::shared_ptr<int>> v;
+  v.push_back(p1);
+  v.push_back(p1);
+  assert(p1.use_count() == 4);
+  p2.reset();
+  assert(p1.use_count() == 3);
+  assert(p2.use_count() == 0);
+
+  v.pop_back();
+  v.pop_back();
+  assert(p1.use_count() == 1);
 }
 
 void weak_ptr() {
@@ -50,5 +69,6 @@ void weak_ptr() {
 
 int main() {
   shared_ptr();
+  shared_ptr_container();
   weak_ptr();
 }
